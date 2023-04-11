a5155318451054357bf47fdd1a62cb33ecab86bc
Geoffrey Meier <grm0809@gmail.com>
2023-04-11 12:17:43 -0500

    finalizing auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '@/styles/globals.css'
-import {ClerkProvider}from '@clerk/nextjs'
+import {ClerkProvider,SignedIn,SignedOut,RedirectToSignIn}from '@clerk/nextjs'
 
 export default function App({ Component, pageProps }) {
 
-  return ( <ClerkProvider {...pageProps}><Component {...pageProps} /></ClerkProvider>)
+  return ( <ClerkProvider {...pageProps}>
+    <SignedIn>
+      <Component {...pageProps} />
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+</ClerkProvider>)
 }
